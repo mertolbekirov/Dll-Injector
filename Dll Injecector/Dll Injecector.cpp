6e0ef819261bde792cbe6dc5bd6df0374e0a60b4
mertolbekirov <mertol2002@gmail.com>
2021-05-13 00:18:53 +0300

    fixed bugs with path input and finished injector

--- Dll Injecector/Dll Injecector.cpp
+++ Dll Injecector/Dll Injecector.cpp
@@ -1,7 +1,78 @@
-
 #include <iostream>
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <string>
+
+#define MAX_LENGTH
+
+DWORD GetProcId(const char* procName) {
+    DWORD procId = 0;
+    HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+    if (hSnap != INVALID_HANDLE_VALUE)
+    {
+        PROCESSENTRY32 procEntry;
+        procEntry.dwSize = sizeof(procEntry);
+
+        if (Process32First(hSnap, &procEntry))
+        {
+            do
+            {
+                if (!_stricmp(procEntry.szExeFile, procName))
+                {
+                    procId = procEntry.th32ProcessID;
+                    break;
+                }
+            } while (Process32Next(hSnap, &procEntry));
+        }
+    }
+    CloseHandle(hSnap);
+    return procId;
+}
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Enter Process Name: ";
+    std::string procNameTmp;
+    std::cin >> procNameTmp;
+    const char* procName = procNameTmp.c_str();
+
+    std::cout << "Enter dll Path: ";
+    std::string dllPathTmp;
+    while (dllPathTmp == "") {
+        getline(std::cin, dllPathTmp);
+    }
+    const char* dllPath = dllPathTmp.c_str();
+
+    DWORD procId = 0;
+
+    while (!procId)
+    {
+        procId = GetProcId(procName);
+        Sleep(30);
+    }
+
+    HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, 0, procId);
+
+    if (hProc && hProc != INVALID_HANDLE_VALUE) {
+        void* loc = VirtualAllocEx(hProc, 0, MAX_PATH, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+
+        WriteProcessMemory(hProc, loc, dllPath, strlen(dllPath) + 1, 0);
+
+        HANDLE hThread = CreateRemoteThread(hProc, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, loc, 0, 0);
+
+        if (hThread)
+        {
+            CloseHandle(hThread);
+        }
+    }
+
+    if (hProc)
+    {
+        CloseHandle(hProc);
+    }
+
+    return 0;
 }
